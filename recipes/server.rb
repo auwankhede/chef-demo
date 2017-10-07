f663838137ffaa935c61f2c66ceb6905c2e0dbae
Amol Wankhede <info.amol@gmail.com>
2017-10-07 06:20:46 +0000

    Use template instead of file

---
+++ recipes/server.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook Name:: apache
+# Recipe:: server
+#
+# Copyright (c) 2017 The Authors, All Rights Reserved.
+
+package 'httpd' do 
+  action :install
+end
+
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+  action :create
+end
+
+
+service 'httpd' do
+  action [ :enable, :start ]
+end
+
